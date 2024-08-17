dc03379a6c743cf4724cb9f77ef62b099ed177a2
elise <elise@DESKTOP-6I7M1T9>
2024-08-17 13:00:09 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static public void show()
+        {
+
+            A a = new A(), b = new B(), c = new C();
+            I ia = new A(), ib = new B(), ic = new C();
+            Console.WriteLine($"{a.P},{b.P},{c.P},{ia.P},{ib.P},{ic.P},");
+        }
+        interface I
+        {
+            int P { get; }
+        }
+
+        class A : I
+        {
+            public virtual int P => 1;
+        }
+
+        class B : A
+        {
+            public override int P => 2;
+        }
+
+        class C : A, I
+        {
+           public int P => 3;
+        }
+        static void Main(string[] args)
+        {
+            show();
+        }
+
+        public static int startIndex(int[] arr)
+        {
+            int minLength = 0;
+            int i = 0;
+            int startIndex = 0;
+
+            while (i < arr.GetLength(0) - 1)
+            {
+                int len = 1;
+                while (i < arr.GetLength(0) - 1 && arr[i] == arr[i + 1])
+                {
+                    len++;
+                    i++;
+                }
+                if (len > minLength)
+                {
+                    startIndex = i - len + 1;
+                    minLength = len;
+                }
+                i++;
+            }
+            return startIndex;
+            /*   if(minLength>=2){
+                   return startIndex;
+               }
+               else{return -1;}*/
+        }
+
+
+    }
+
+
+    public static class primeoooo
+    {
+        public static bool IsPrime(this int a) {
+            
+            bool prost = true;
+
+            for (int i = 2; i <= a / 2; i++)
+            {
+                if (a % i == 0)
+                {
+                    prost = false;
+                    break;
+                }
+            }
+            return prost;
+        }
+    }
+}
---
+++ ConsoleApp1/StarOfDeath.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class StarOfDeath
+    {
+        double power;
+        double charge;
+        string name;
+        string[] weapons = { "Истребитель", "Рарушитель", "Лазер" };
+
+
+        public string this[int index]
+        {
+            get  => weapons[index];  
+        }
+
+        public StarOfDeath() {
+            power = default(double); 
+            charge = default(double);
+            name = default(string);
+        }
+
+
+        public StarOfDeath(double power, double size, string name) { 
+            this.power = power;
+            this.charge = size;
+            this.name = name;
+        }
+
+        public void Сharge(double amount)
+        {
+            if (amount <= 0) Console.WriteLine("Нет энергии");
+            if ((this.charge += amount) > 5000.0)
+            {
+                this.charge = 5000;
+                Console.WriteLine("Устройство заряжено до полного бака");
+            }
+            else this.charge -= amount;
+            this.charge += amount;
+            Console.WriteLine("Устройство заряжено до " + this.charge + " Ампер");
+
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine("Выстрелел");
+            this.charge -= 1000;
+        }
+
+        public void CurrCharge()
+        {
+            Console.WriteLine("Текущий заряд Звезды Смерти равен " + this.charge);
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public static StarOfDeath operator +(StarOfDeath a,StarOfDeath b) { 
+            StarOfDeath ret = new StarOfDeath();
+            ret.power = a.power + b.power;
+            ret.charge = a.charge + b.charge;
+            ret.name = a.name + b.name+"!!!!<3";
+            return ret;
+        }
+
+        public static StarOfDeath operator +(StarOfDeath a, double b)
+        {
+            StarOfDeath ret = new StarOfDeath();
+           
+            ret.charge = a.charge + b;
+            return ret;
+        }
+
+        
+    }
+    
+}
